05cbd30cf584c4a4fcd2fe873a28be720579376d
kertalo <koksharov-kir@yandex.ru>
2023-10-08 11:52:51 +0300

    Add project files.

---
+++ AreaOfFigure.Tests/FigureTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AreaOfFigure.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        double e = 0.0001;
+
+        [TestMethod]
+        public void TriangleArea_3_4_5()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double expected = 6;
+            Triangle triangle = new Triangle(a, b, c);
+            double actual = triangle.GetArea();
+            Assert.IsTrue(Math.Abs(expected - actual) < e);
+        }
+
+        [TestMethod]
+        public void TriangleArea_5_8_6()
+        {
+            double a = 5;
+            double b = 8;
+            double c = 6;
+            double expected = 14.98124;
+            Triangle triangle = new Triangle(a, b, c);
+            double actual = triangle.GetArea();
+            Assert.IsTrue(Math.Abs(expected - actual) < e);
+        }
+
+        [TestMethod]
+        public void TriangleIsRectangular_3_5_4()
+        {
+            double a = 3;
+            double b = 5;
+            double c = 4;
+            Triangle triangle = new Triangle(a, b, c);
+            Assert.IsTrue(triangle.IsRectangularTriangle());
+        }
+
+        [TestMethod]
+        public void TriangleIsRectangular_3_5_6()
+        {
+            double a = 3;
+            double b = 5;
+            double c = 6;
+            Triangle triangle = new Triangle(a, b, c);
+            Assert.IsTrue(!triangle.IsRectangularTriangle());
+        }
+    }
+
+    [TestClass]
+    public class CircleTests
+    {
+        double e = 0.0001;
+
+        [TestMethod]
+        public void CircleArea_3()
+        {
+            double r = 3;
+            double expected = 28.27433;
+            Circle circle = new Circle(r);
+            double actual = circle.GetArea();
+            Assert.IsTrue(Math.Abs(expected - actual) < e);
+        }
+
+        [TestMethod]
+        public void CircleArea_1()
+        {
+            double r = 1;
+            double expected = Math.PI;
+            Circle circle = new Circle(r);
+            double actual = circle.GetArea();
+            Assert.IsTrue(Math.Abs(expected - actual) < e);
+        }
+    }
+}
---
+++ Figure.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfFigure
+{
+    public abstract class Figure
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        private double a, b, c;
+        private double e = Double.Epsilon;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.a = side1;
+            this.b = side2;
+            this.c = side3;
+        }
+
+        public bool IsRectangularTriangle()
+        {
+            if (a > b && a > c)
+                return Math.Abs(Math.Pow(b, 2) +
+                    Math.Pow(c, 2) - Math.Pow(a, 2)) < e;
+            if (b > a && b > c)
+                return Math.Abs(Math.Pow(a, 2) +
+                    Math.Pow(c, 2) - Math.Pow(b, 2)) < e;
+            return Math.Abs(Math.Pow(a, 2) +
+                Math.Pow(b, 2) - Math.Pow(c, 2)) < e;
+        }
+
+        public override double GetArea()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
